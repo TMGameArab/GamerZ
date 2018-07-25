c3a84a42db84958cdcf8b50391a8705b3e8d3583
TMGameArab <13922988+TMGameArab@users.noreply.github.com>
2018-07-25 04:54:23 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -705,69 +705,6 @@ client.on("guildMemberAdd", (member) => {
     });
 
 });
- 
-
-client.on("message", message => {
-  let command = message.content.split(" ")[0];
-  if (command === "**mute") {
-          if(!message.channel.guild) return message.reply(':x: اسف لكن هذا الامر للسيرفرات فقط ');
-          if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(`انت لست ادمن`).catch(console.error)
-  let user = message.mentions.users.first();
-  let log = client.channels.find('name', 'audit-log');
-  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply(" لا يوجد رتبة الميوت 'Muted' ").catch(console.error);
-  if (!log) return message.reply("لا يوجد الروم المراد ارسال المعلومات له 'audit-log'");
-  if (message.mentions.users.size < 1) return message.reply(' يجب عليك المنشن اولاً ');
-  var embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .addField(' Mute ', ' | :white_check_mark: |')
-    .addField('تم اعطاء الميوت ل',` ${user.username}#${user.discriminator}`)
-    .addField('السبب', 'تعكير نظام الشات')
-    .addField('بواسطة:',` ${message.author.username}#${message.author.discriminator}`)
-  ({embed: embed});
-
-  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply(' لا يوجد لدي برمشن Manage Roles ').catch(console.error);
-
-  if (message.guild.member(user).roles.has(muteRole.id)) {
-      client.channels.get(log.id).send({embed}).catch(console.error);
-  } else {
-    message.guild.member(user).addRole(muteRole).then(() => {
-      client.channels.get(log.id).send({embed}).catch(console.error);
-    });
-  }
-
-};
-
-if (command === "**unmute") {
-          if(!message.channel.guild) return message.reply(':x: اسف لكن هذا الامر للسيرفرات فقط ');
-          if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(`انت لست ادمن`).catch(console.error)
-  let user = message.mentions.users.first();
-  let log = client.channels.find('name', 'audit-log');
-  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply(" لا يوجد رتبة الميوت 'Muted' ");
-  if (!log) return message.reply("لا يوجد الروم المراد ارسال المعلومات له 'audit-log'");
-  if (message.mentions.users.size < 1) return message.reply(' يجب عليك المنشن اولاً ');
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .addField('UnMute ', ' | :white_check_mark: |')
-    .addField('تم فك الميوت عن', `${user.username}#${user.discriminator}`)
-    .addField('بواسطة:', `${message.author.username}#${message.author.discriminator}`)
-  ({embed: embed});
-
-  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply(' لا يوجد لدي برمشن Manage Roles ');
-
-  if (message.guild.member(user).removeRole(muteRole.id)) {
-      client.channels.get(log.id).send({embed});
-  } else {
-    message.guild.member(user).removeRole(muteRole).then(() => {
-      client.channels.get(log.id).send({embed});
-    });
-  }
-
-};
-
-
-});
 
 client.on('message', message => {
 if (message.content.startsWith(prefix+"role")) {
