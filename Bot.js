f6993d0f4c2e12d34393d7dc83c47c259c50d836
TMGameArab <13922988+TMGameArab@users.noreply.github.com>
2018-07-30 02:21:27 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -24,12 +24,6 @@ client.on('message', msg => {
   }
 });
 //--------------------------------------------
-client.on('message', msg => {
-  if (msg.content === 'رابط') {
-    msg.reply('Check Your DM :envelope_with_arrow:');
-  }
-});
-//--------------------------------------------
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
 
@@ -59,18 +53,6 @@ client.on('message', message => {
 **`)
       message.author.sendEmbed(Embed11)
     }
-});
-//--------------------------------------------
-client.on('message', msg => {
-  if (msg.content === 'مازن') {
-    msg.reply('ول ول ول ول ول ول ول ول ول ول ول ول ول');
-  }
-});
-//--------------------------------------------
-client.on('message', msg => {
-  if (msg.content === 'عبقر') {
-    msg.reply('انا جالي كود');
-  }
 });
 //--------------------------------------------
 client.on('message', msg => {
@@ -724,41 +706,41 @@ var prefix = "**";
 if (!message.content.startsWith(prefix)) return;
 var args = message.content.split(' ').slice(1);
 var argresult = args.join(' ');
-if (message.author.id == 294155759741304833) return;
+if (message.author.id == 404367572063092736) return;
 if (message.content.startsWith(prefix + 'playing')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
 client.user.setGame(argresult);
  message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
 } else
 
 
 if (message.content.startsWith(prefix + 'streem')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
 client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
  message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
 } else
 
 if (message.content.startsWith(prefix + 'setname')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
 client.user.setUsername(argresult).the
 message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
 return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
 } else
  
 if (message.content.startsWith(prefix + 'setavatar')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
 client.user.setAvatar(argresult);
  message.channel.sendMessage(`**${argresult}** : تم تغ��ر صورة البوت`);
 } else
 
 
 if (message.content.startsWith(prefix + 'watching')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
  client.user.setActivity(argresult, {type : 'watching'});
 message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
 }
 if (message.content.startsWith(prefix + 'listening')) {
-if (message.author.id !== '294155759741304833') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+if (message.author.id !== '404367572063092736') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
 client.user.setActivity(argresult, {type : 'listening'});
 message.channel.sendMessage(`**${argresult}**: تم تغير الاستماع الي`)
 }
